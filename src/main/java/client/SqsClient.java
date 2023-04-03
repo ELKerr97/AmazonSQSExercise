7516c76e3d2c8263eb46bcf3122ff2750e5bfbee
Ethan Kerr <elkerr97@gmail.com>
2023-04-03 11:33:08 -0600

    Finish.

--- src/main/java/client/SqsClient.java
+++ src/main/java/client/SqsClient.java
@@ -1,3 +1,5 @@
+package client;
+
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
@@ -7,7 +9,7 @@ import com.amazonaws.services.sqs.model.SendMessageResult;
 public class SqsClient {
   public static void main(String[] args) {
 
-    String messageBody = "Message from SQS Client";
+    String messageBody = "Hello from IntelliJ!!";
     String queueUrl = "https://sqs.us-east-1.amazonaws.com/771290919915/SQSExerciseQueue";
 
     SendMessageRequest send_msg_request = new SendMessageRequest()
